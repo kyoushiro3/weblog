e4ed999a0bee44c8af8b179269db3512f4983282
kyoushiro3 <starkjarvis99@gmail.com>
2024-07-26 03:58:53 +0800

    test deployment

--- src/app/api/blogs/getBlog.ts
+++ src/app/api/blogs/getBlog.ts
@@ -1,15 +1,15 @@
-export const getBlogs = async () =>{
-    try{
-        const res = await fetch("http://localhost:3000/api/blogs", {
-          cache: "no-store"
-        })
-        if(!res.ok){
-          throw new Error("Failed to fetch blogs.")
-      }
-      return res.json();
+export const getBlogs = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/blogs");
 
+    if (!res.ok) {
+      throw new Error("Failed to fetch blogs");
     }
-    catch(error){
-      console.log("Error loadings blogs: ",error)
-    }
+
+    const data = await res.json();
+    return { blogs: data.blogs }; 
+  } catch (error) {
+    console.error(error);
+    return { blogs: [] };
   }
+};
