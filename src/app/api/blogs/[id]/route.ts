1b43a67d2f19812e1ddc884fc6bdbbdbcdc8c843
kyoushiro3 <starkjarvis99@gmail.com>
2024-07-26 02:06:08 +0800

    fixed: api route

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -14,20 +14,20 @@ export async function PUT(request: any,  { params }: { params: { id: string } })
     await Blog.findByIdAndUpdate(id, { title, description, catogory, date, time })
     return NextResponse.json({message: "Blog updated"}, {status:200})
 }
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const { id } = req.query;
-        await connectMongoDB();
-        const blog = await Blog.findById(id);
-        if (!blog) {
-            return NextResponse.json({ message: "Blog not found" }, { status: 404 });
-        }
-        return NextResponse.json({ blog }, { status: 200 });
-            } catch (error) {
-        console.error("Error fetching blog:", error);
-        return NextResponse.json({ message: "Failed to fetch blog", error: error.message }, { status: 500 });
-    }
-  }
+// export async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     try {
+//         const { id } = req.query;
+//         await connectMongoDB();
+//         const blog = await Blog.findById(id);
+//         if (!blog) {
+//             return NextResponse.json({ message: "Blog not found" }, { status: 404 });
+//         }
+//         return NextResponse.json({ blog }, { status: 200 });
+//             } catch (error) {
+//         console.error("Error fetching blog:", error);
+//         return NextResponse.json({ message: "Failed to fetch blog", error: error.message }, { status: 500 });
+//     }
+//   }
 // export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
 //     try {
 //         const { id } = params;
