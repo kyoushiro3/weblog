25c23eac0c248749ebd854484d90eba5702a19ca
kyoushiro3 <starkjarvis99@gmail.com>
2024-07-26 01:58:18 +0800

    fixed packages.json

--- src/app/components/nav/Footer.tsx
+++ src/app/components/nav/Footer.tsx
@@ -4,17 +4,17 @@ import React from "react"
 const Footer = () => {
   return (
     <div className="w-full overflow-hidden">
-      <hr className="h-px w-full my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
+      <hr className="h-px w-full my-8 bg-black border-0 dark:bg-gray-700"/>
       <div className="w-full flex items-center justify-center">
-        <p className="text-gray-500 dark:text-gray-400">Made by</p>
+        <p className="text-gray-500 dark:text-gray-400 text-sm">Made by</p>
         <Link
           href="https://github.com/kyoushiro3"
-          className="ml-2 underline underline-offset-2"
+          className="ml-2 underline underline-offset-2 text-sm"
           target="_blank"
         >
           Kim Darren
         </Link>
-        <p className="text-gray-500 dark:text-gray-400">.</p>
+        <p className="text-gray-500 dark:text-gray-400 text-sm">.</p>
       </div>
     </div>
   )
